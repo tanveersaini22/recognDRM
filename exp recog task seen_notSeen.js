6e683e8a8c7883379deb260b1a9b6a63821307fe
tanveersaini22 <tanny2203@gmail.com>
2026-04-07 02:39:23 +0530

    Update exp recog task seen_notSeen.js

--- exp recog task seen_notSeen.js
+++ exp recog task seen_notSeen.js
@@ -269,7 +269,7 @@ async function experimentInit() {
     ori : 0.0, 
     pos : [0, 0.085], 
     draggable: false,
-    size : [0.6, 0.6],
+    size : [0.5, 0.5],
     color : new util.Color([1,1,1]), opacity : undefined,
     flipHoriz : false, flipVert : false,
     texRes : 128.0, interpolate : true, depth : 0.0 
@@ -822,7 +822,7 @@ function taskRoutineBegin(snapshot) {
     routineTimer.reset();
     taskMaxDurationReached = false;
     // update component parameters for each repeat
-    imageStim.setImage(imagePath);
+    imageStim.setImage('default.png');
     // reset seen to account for continued clicks & clear times on/off
     seen.reset()
     // reset notSeen to account for continued clicks & clear times on/off
